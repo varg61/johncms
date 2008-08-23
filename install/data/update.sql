f40dc6bd8ed6a44870e93c1e415e6502f27cf464
Oleg Kasyanov <Oleg Kasyanov@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2008-08-23 07:30:10 +0000

    [dev]  Подправленный стиль [dev]  Рабочий патч для обновления с предыдущей версии

--- install/data/update.sql
+++ install/data/update.sql
@@ -3,7 +3,7 @@
 --
 DROP TABLE IF EXISTS `ban`;
 DROP TABLE IF EXISTS `moder`;
-
+DROP TABLE IF EXISTS `ban_ip`;
 
 --
 -- Создаем таблицу настроек
@@ -15,12 +15,44 @@ CREATE TABLE `cms_settings` (
   PRIMARY KEY  (`key`(30))
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `cms_settings`
+--
+INSERT INTO `cms_settings` (`key`, `val`) VALUES
+('nickadmina', ''),
+('emailadmina', ''),
+('nickadmina2', ''),
+('sdvigclock', '0'),
+('copyright', ''),
+('homeurl', ''),
+('rashstr', 'txt'),
+('admp', 'panel'),
+('flsz', '1000'),
+('gzip', '0'),
+('rmod', '0'),
+('fmod', '0'),
+('gb', '0'),
+('clean_time', '0'),
+('mod_reg', '1'),
+('mod_reg_msg', 'Регистрация временно закрыта'),
+('mod_forum', '1'),
+('mod_forum_msg', 'Форум временно закрыт'),
+('mod_chat', '1'),
+('mod_chat_msg', 'Чат временно закрыт'),
+('mod_guest', '1'),
+('mod_guest_msg', 'Гостевая временно закрыта'),
+('mod_lib', '1'),
+('mod_lib_msg', 'Библиотека временно закрыта'),
+('mod_gal', '1'),
+('mod_gal_msg', 'Галерея временно закрыта'),
+('mod_down', '1'),
+('mod_down_msg', 'Загрузки временно закрыты');
 
 --
--- Создаем таблицу `ban_ip`
+-- Создаем таблицу `cms_ban_ip`
 --
-DROP TABLE IF EXISTS `ban_ip`;
-CREATE TABLE `ban_ip` (
+DROP TABLE IF EXISTS `cms_ban_ip`;
+CREATE TABLE `cms_ban_ip` (
   `ip` int(11) NOT NULL default '0',
   `ban_type` tinyint(4) NOT NULL default '0',
   `link` varchar(100) NOT NULL,
@@ -30,29 +62,37 @@ CREATE TABLE `ban_ip` (
   PRIMARY KEY  (`ip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Создаем таблицу `cms_ban_users`
+--
+DROP TABLE IF EXISTS `cms_ban_users`;
+CREATE TABLE `cms_ban_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `user_id` int(11) NOT NULL,
+  `ban_time` int(11) NOT NULL,
+  `ban_while` int(11) NOT NULL,
+  `ban_type` tinyint(4) NOT NULL default '1',
+  `ban_who` varchar(30) NOT NULL,
+  `ban_reason` text NOT NULL,
+  `ban_raz` varchar(30) NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `ban_time` (`ban_time`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Модифицируем таблицу "settings"
---
-ALTER TABLE `settings` CHANGE `gb` `gb` BOOL NOT NULL DEFAULT '0';
-ALTER TABLE `settings` CHANGE `fmod` `fmod` BOOL NOT NULL DEFAULT '0';
-ALTER TABLE `settings` CHANGE `rmod` `rmod` BOOL NOT NULL DEFAULT '0';
-ALTER TABLE `settings` CHANGE `gzip` `gzip` BOOL NOT NULL DEFAULT '0';
-ALTER TABLE `settings` CHANGE `sdvigclock` `sdvigclock` TINYINT NOT NULL DEFAULT '0';
-
---
--- Модифицируем таблицу "guest"
+-- Модифицируем таблицу `guest`
 --
 ALTER TABLE `guest` ADD `adm` BOOL NOT NULL DEFAULT '0' AFTER `id` ;
 ALTER TABLE `guest` ADD INDEX ( `adm` ) ;
 
 --
--- Модифицируем таблицу "lib"
+-- Модифицируем таблицу `lib`
 --
 ALTER TABLE `lib` CHANGE `moder` `moder` BOOL NOT NULL DEFAULT '0';
 
 --
--- Модифицируем таблицу "users"
+-- Модифицируем таблицу `users`
 --
 ALTER TABLE `users` DROP `ban`;
 ALTER TABLE `users` DROP `why`;
