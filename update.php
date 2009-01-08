68121bcebb8538f00f1a5f339efb5661c1e5d537
Oleg Kasyanov <Oleg Kasyanov@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2009-01-08 21:31:58 +0000

    [dev]   Рабочий конвертер с предыдущей версии [dev]   Подправил ошибку с тэгами <br /> на вновь создаваемых темах.

--- update.php
+++ update.php
@@ -11,8 +11,7 @@ echo "<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.or
 <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en'>
 <head>
 <meta http-equiv='content-type' content='application/xhtml+xml; charset=utf-8'/>";
-echo "<link rel='shortcut icon' href='ico.gif' />
-<title>Обновление системы</title>
+echo "<title>Обновление системы</title>
 <style type='text/css'>
 body { font-weight: normal; font-family: Century Gothic; font-size: 12px; color: #FFFFFF; background-color: #000033}
 a:link { text-decoration: underline; color : #D3ECFF}
@@ -27,8 +26,8 @@ a:hover { text-decoration: none; font-size: 12px; color : #E4F992 }
 echo '<big><b>JohnCMS v.2.0.0</b></big> Обновление<hr />';
 
 // Подключаемся к базе данных
-require_once ("../incfiles/db.php");
-require_once ("../incfiles/func.php");
+require_once ("incfiles/db.php");
+require_once ("incfiles/func.php");
 $connect = mysql_connect($db_host, $db_user, $db_pass) or die('cannot connect to server</body></html>');
 mysql_select_db($db_name) or die('cannot connect to db');
 mysql_query("SET NAMES 'utf8'", $connect);
@@ -36,14 +35,48 @@ mysql_query("SET NAMES 'utf8'", $connect);
 $do = isset($_GET['do']) ? $_GET['do'] : '';
 switch ($do)
 {
-    case 'forum':
-        echo '<b><u>Обновляем форум</u></b><br />';
+    case 'step1':
+        echo '<b><u>Подготовка таблиц</u></b><br />';
+        // Создаем таблицу меток прочтения
+        mysql_query("DROP TABLE IF EXISTS `cms_forum_rdm`");
+        mysql_query("CREATE TABLE `cms_forum_rdm` (
+        `topic_id` int(11) NOT NULL,
+        `user_id` int(11) NOT NULL,
+        `time` int(11) NOT NULL,
+        PRIMARY KEY  (`topic_id`,`user_id`),
+        KEY `time` (`time`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+        echo '<span class="green">OK</span> таблица меток готова<br />';
+        // Модифицируем таблицу `users`
+        mysql_query("ALTER TABLE `users` ADD `digest` TINYINT NOT NULL DEFAULT '1'");
+        mysql_query("ALTER TABLE `users` ADD `skin` varchar(20) NOT NULL");
+        mysql_query("ALTER TABLE `users` CHANGE `offsm` `offsm` TINYINT( 1 ) NOT NULL DEFAULT '1'");
+        mysql_query("ALTER TABLE `users` CHANGE `offtr` `offtr` TINYINT( 1 ) NOT NULL DEFAULT '1'");
+        mysql_query("ALTER TABLE `users` CHANGE `pereh` `pereh` TINYINT( 1 ) NOT NULL DEFAULT '1'");
+        echo '<span class="green">OK</span> таблица users готова<br />';
+        // Конвертируем пользовательские настройки
+        $req = mysql_query("SELECT `id` FROM `users`");
+        while ($res = mysql_fetch_array($req))
+        {
+            mysql_query("UPDATE `users` SET `offsm`='1', `offtr`='1', `pereh`='1' WHERE `id`='" . $res['id'] . "';");
+        }
+        mysql_query("INSERT INTO `cms_settings` SET `key`='skindef', `val`='default';");
+		echo '<span class="green">OK</span> настройки готовы<br />';
+        echo '<hr /><a href="update.php?do=step2">Продолжить</a>';
+        break;
 
+    case 'step2':
+        echo '<b><u>Удаление меток форума</u></b><br />';
+        echo '<p>Внимание!<br />На больших форумах, данная процедура может длиться довольно долго.</p>';
         // Удаляем метки (l)
-        //$req = mysql_query("DELETE FROM `forum` WHERE `type`='l';");
-        //mysql_query("OPTIMIZE TABLE `forum`;");
-        //echo '<span class="green">OK</span> метки удалены.<br />';
+        $req = mysql_query("DELETE FROM `forum` WHERE `type`='l';");
+        echo '<span class="green">OK</span> метки удалены.<br />';
+        echo '<hr /><a href="update.php?do=step3">Продолжить</a>';
+        break;
 
+    case 'step3':
+        echo '<b><u>Конвертация форума</u></b><br />';
+        echo '<p>Внимание!<br />На больших форумах, данная процедура может длиться довольно долго.</p>';
         $req = mysql_query("SELECT * FROM `forum` WHERE `type`='m';");
         while ($res = mysql_fetch_array($req))
         {
@@ -59,31 +92,15 @@ switch ($do)
 
             $text = mysql_real_escape_string($text);
             mysql_query("UPDATE `forum` SET
-			`text`='" . $text . "'
-			WHERE `id`='" . $res['id'] . "';");
+        `text`='" . $text . "'
+        WHERE `id`='" . $res['id'] . "';");
         }
-
-        /*
-
-        // Создаем таблицу меток прочтения
-        mysql_query("DROP TABLE IF EXISTS `cms_forum_rdm`;");
-        mysql_query("CREATE TABLE `cms_forum_rdm` (
-        `topic_id` int(11) NOT NULL,
-        `user_id` int(11) NOT NULL,
-        `time` int(11) NOT NULL,
-        PRIMARY KEY  (`topic_id`,`user_id`),
-        KEY `time` (`time`)
-        ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
-        echo '<span class="green">OK</span> таблица меток создана.<br />';
-
-        */
-
-        // Создаем поле users.digest
-        mysql_query("ALTER TABLE `users` ADD `digest` TINYINT NOT NULL DEFAULT '1';");
-        echo '<hr /><a href="up_160_200.php?do=final">Продолжить</a>';
+        echo '<span class="green">OK</span> конвертация завершена.<br />';
+		echo '<hr /><a href="update.php?do=final">Продолжить</a>';
         break;
 
     case 'final':
+        mysql_query("OPTIMIZE TABLE `forum`;");
         echo '<b><span class="green">Поздравляем!</span></b><br />Процедура обновления успешно завершена.<br />Не забудьте удалить папку /install';
         echo '<hr /><a href="../../index.php">На сайт</a>';
         break;
@@ -97,7 +114,7 @@ switch ($do)
         echo '<li></li>';
         echo '</ul></p>';
 
-        echo '<hr />Вы уверены?<br /><a href="up_160_200.php?do=forum">Продолжить</a>';
+        echo '<hr />Вы уверены?<br /><a href="update.php?do=step1">Продолжить</a>';
 }
 
 echo '</body>
