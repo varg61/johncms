5a2cc7dc866939d2623a224d5e942bf1e4afef7e
Oleg Kasyanov <Oleg Kasyanov@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2009-02-09 14:18:04 +0000

    [mod]   Немного оптимизирован код чата. В 2 раза увеличена скорость работы [dev]   В чате применено чередование цветов

--- chat/hall.php
+++ chat/hall.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 ////////////////////////////////////////////////////////////////////////////////
 // JohnCMS                             Content Management System              //
@@ -24,10 +25,12 @@ $_SESSION['intim'] = '';
 $q = mysql_query("select * from `chat` where type='r' order by realid ;");
 while ($mass = mysql_fetch_array($q))
 {
-    echo '<div class="menu"><a href="index.php?id=' . $mass['id'] . '">' . $mass['text'] . '</a> (' . wch($mass['id']) . ')</div>';
+    echo ceil(ceil($i / 2) - ($i / 2)) == 0 ? '<div class="list1">' : '<div class="list2">';
+    echo '<a href="index.php?id=' . $mass['id'] . '">' . $mass['text'] . '</a> (' . wch($mass['id']) . ')</div>';
+    ++$i;
 }
 echo '<div class="bmenu">В прихожей (' . wch() . ')</div>';
-echo '<p><a href="who.php">Кто в чате? (' . wch(0,1) . ')</a><br/>';
+echo '<p><a href="who.php">Кто в чате? (' . wch(0, 1) . ')</a><br/>';
 echo '<a href="index.php?act=moders&amp;id=' . $id . '">Модераторы</a><br/>';
 echo "<a href='../str/usset.php?act=chat'>Настройки чата</a></p>";
 require_once ('../incfiles/end.php');
