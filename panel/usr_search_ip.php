adb81d6843b09fe24457bafb6c9300c43065b1b5
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2010-06-08 17:15:57 +0000

    [dev]   Исправлена ошибка в SQL запросе поиска по IP

--- panel/usr_search_ip.php
+++ panel/usr_search_ip.php
@@ -75,15 +75,16 @@ if ($search && !$error) {
     ////////////////////////////////////////////////////////////
     // Выводим результаты поиска                              //
     ////////////////////////////////////////////////////////////
-    echo '<div class="phdr">Результаты запроса '.$ip1.' '.$ip2.'</div>';
+    echo '<div class="phdr">Результаты запроса</div>';
     $total = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_users_iphistory` WHERE `user_ip` BETWEEN $ip1 AND $ip2"), 0);
     if ($total) {
-        $req = mysql_query("SELECT `cms_users_iphistory`.`user_ip`, `users`.*
+        $req = mysql_query("SELECT `cms_users_iphistory`.`user_ip` AS `ip`, `users`.`name`, `users`.`rights`, `users`.`lastdate`, `users`.`sex`, `users`.`status`, `users`.`datereg`, `users`.`id`, `users`.`browser`
         FROM `cms_users_iphistory` LEFT JOIN `users` ON `cms_users_iphistory`.`user_id` = `users`.`id`
         WHERE `cms_users_iphistory`.`user_ip` BETWEEN $ip1 AND $ip2
         ORDER BY `ip` ASC, `name` ASC LIMIT $start, $kmess");
         while ($res = mysql_fetch_assoc($req)) {
             echo $i % 2 ? '<div class="list2">' : '<div class="list1">';
+            
             echo show_user($res, array('iphist' => 1));
             echo '</div>';
             ++$i;
