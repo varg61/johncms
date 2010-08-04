dae6378f41c9ccff0e8da1538c5ad39d7f1a60ee
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2010-08-04 12:31:16 +0000

    [dev]   Исправлены ссылки на контакты и игнор

--- str/my_cabinet.php
+++ str/my_cabinet.php
@@ -46,7 +46,7 @@ switch ($act) {
         $count_sentunread = mysql_result(mysql_query("SELECT COUNT(*) FROM `privat` WHERE `author` = '$login' AND `type` = 'out' AND `chit` = 'no'"), 0);
         $count_files = mysql_result(mysql_query("SELECT COUNT(*) FROM `privat` WHERE `user` = '$login' AND `type` = 'in' AND `attach` != ''"), 0);
         echo '<div class="menu"><p><h3><img src="../images/mail.png" width="16" height="16" class="left" />&#160;' . $lng_cab['my_mail'] . '</h3><ul>' .
-            '<li><a href="pradd.php?act=in">' . $lng_cab['received'] . '</a>&#160;(' . $count_mail . ($count_newmail ? '&#160;/&#160;<span class="red"><a href="str/pradd.php?act=in&amp;new">+' . $count_newmail . '</a></span>' : '') . ')</li>' .
+            '<li><a href="pradd.php?act=in">' . $lng_cab['received'] . '</a>&#160;(' . $count_mail . ($count_newmail ? '&#160;/&#160;<span class="red"><a href="pradd.php?act=in&amp;new">+' . $count_newmail . '</a></span>' : '') . ')</li>' .
             '<li><a href="pradd.php?act=out">' . $lng_cab['sent'] . '</a>&#160;(' . $count_sentmail . ($count_sentunread ? '&#160;/&#160;<span class="red">' . $count_sentunread . '</span>' : '') . ')</li>';
         if (!$ban['1'] && !$ban['3'])
             echo '<p><form action="pradd.php?act=write" method="post"><input type="submit" value=" ' . $lng['write'] . ' " /></form></p>';
@@ -54,8 +54,8 @@ switch ($act) {
         $count_contacts = mysql_result(mysql_query("SELECT COUNT(*) FROM `privat` WHERE `me` = '$login' AND `cont` != ''"), 0);
         $count_ignor = mysql_result(mysql_query("SELECT COUNT(*) FROM `privat` WHERE `me` = '$login' AND `ignor` != ''"), 0);
         echo '</ul><h3><img src="../images/contacts.png" width="16" height="16" class="left" />&#160;' . $lng['contacts'] . '</h3><ul>' .
-            '<li><a href="str/cont.php">' . $lng['contacts'] . '</a>&#160;(' . $count_contacts . ')</li>' .
-            '<li><a href="str/ignor.php">' . $lng['blocking'] . '</a>&#160;(' . $count_ignor . ')</li>' .
+            '<li><a href="cont.php">' . $lng['contacts'] . '</a>&#160;(' . $count_contacts . ')</li>' .
+            '<li><a href="ignor.php">' . $lng['blocking'] . '</a>&#160;(' . $count_ignor . ')</li>' .
             '</ul></p></div>';
         // Блок настроек
         echo '<div class="bmenu"><p><h3><img src="../images/settings.png" width="16" height="16" class="left" />&#160;' . $lng_cab['my_settings'] . '</h3><ul>' .
