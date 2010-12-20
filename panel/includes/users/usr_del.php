77d528465f9780f7902d92516282077b7fdf92df
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2010-12-20 20:38:58 +0000

    [dev]   Доработано удаление пользователей

--- panel/includes/users/usr_del.php
+++ panel/includes/users/usr_del.php
@@ -35,22 +35,22 @@ if ($id && $id != $user_id) {
     $error = $lng['error_user_not_specified'];
 }
 if (!$error) {
-    //TODO: После доработки модулей, переделать запросы на User ID и чистку Чата
-    $req_a = mysql_query("SELECT * FROM `gallery` WHERE `type` = 'al' AND `user` = '1' AND `avtor` = '" . $user['name'] . "' LIMIT 1");
-    if (mysql_num_rows($req_a)) {
-        $res_a = mysql_fetch_assoc($req_a);
-        $album = 1;
-        $images_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `gallery` WHERE `refid` = '" . $res_a['id'] . "' AND `type` = 'ft'"), 0);
-    } else {
-        $album = 0;
-        $images_count = 0;
-    }
+    // Считаем комментарии в галерее
     $comm_gal = mysql_result(mysql_query("SELECT COUNT(*) FROM `gallery` WHERE `avtor` = '" . $user['name'] . "' AND `type` = 'km'"), 0);
+    // Считаем комментарии в библиотеке
     $comm_lib = mysql_result(mysql_query("SELECT COUNT(*) FROM `lib` WHERE `avtor` = '" . $user['name'] . "' AND `type` = 'komm'"), 0);
+    // Считаем комментарии к загрузкам
     $comm_dl = mysql_result(mysql_query("SELECT COUNT(*) FROM `download` WHERE `avtor` = '" . $user['name'] . "' AND `type` = 'komm'"), 0);
-    $comm_count = $comm_gal + $comm_lib + $comm_dl;
+    // Считаем посты в личных гостевых
+    $comm_gb = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_users_guestbook` WHERE `user_id` = '" . $user['id'] . "'"), 0);
+    // Считаем комментарии в личных альбомах
+    $comm_al = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_comments` WHERE `user_id` = '" . $user['id'] . "'"), 0);
+    $comm_count = $comm_gal + $comm_lib + $comm_dl + $comm_gb + $comm_al;
+    // Считаем посты в Гостевой
     $guest_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `guest` WHERE `user_id` = '" . $user['id'] . "'"), 0);
+    // Считаем созданные темы на Форуме
     $forumt_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `forum` WHERE `user_id` = '" . $user['id'] . "' AND `type` = 't' AND `close` != '1'"), 0);
+    // Считаем посты на Форуме
     $forump_count = mysql_result(mysql_query("SELECT COUNT(*) FROM `forum` WHERE `user_id` = '" . $user['id'] . "' AND `type` = 'm'  AND `close` != '1'"), 0);
     echo '<div class="phdr"><a href="index.php"><b>' . $lng['admin_panel'] . '</b></a> | ' . $lng['user_del'] . '</div>';
     // Выводим краткие данные
@@ -60,48 +60,61 @@ if (!$error) {
     )) . '</p></div>';
     switch ($mod) {
         case 'del':
-            // Удаляем личный альбом
-            if ($album && isset($_POST['gallery'])) {
-                if ($images_count) {
-                    $req = mysql_query("SELECT COUNT(*) FROM `gallery` WHERE `refid` = '" . $res_a['id'] . "' AND `type` = 'ft'");
-                    while ($res = mysql_fetch_assoc($req)) {
-                        if (file_exists('../gallery/foto/' . $res['name']))
-                            unlink('../gallery/foto/' . $res['name']);
-                    }
+            /*
+            -----------------------------------------------------------------
+            Удаляем личные данные
+            -----------------------------------------------------------------
+            */
+            // Удаляем личную Гостевую
+            mysql_query("DELETE FROM `cms_users_guestbook` WHERE `sub_id` = '" . $user['id'] . "'");
+            // Удаляем личные Фотоальбомы
+            $req = mysql_query("SELECT * FROM `cms_album_files` WHERE `user_id` = '" . $user['id'] . "'");
+            if (mysql_num_rows($req)) {
+                while ($res = mysql_fetch_assoc($req)) {
+                    // Удаляем файлы картинок
+                    @unlink('../files/users/album/' . $user['id'] . '/' . $res['img_name']);
+                    @unlink('../files/users/album/' . $user['id'] . '/' . $res['tmb_name']);
+                    // Удаляем комментарии к файлам
+                    mysql_query("DELETE FROM `cms_album_comments` WHERE `sub_id` = '" . $res['id'] . "'");
                 }
-                mysql_query("DELETE FROM `gallery` WHERE `refid` = '" . $res_a['id'] . "'");
-                mysql_query("DELETE FROM `gallery` WHERE `id` = '" . $res_a['id'] . "'");
-                mysql_query("OPTIMIZE TABLE `gallery`");
             }
+            rmdir('../files/users/album/' . $user['id']);
+            mysql_query("DELETE FROM `cms_album_files` WHERE `user_id` = '" . $user['id'] . "'");
+            mysql_query("DELETE FROM `cms_album_cat` WHERE `user_id` = '" . $user['id'] . "'");
+            mysql_query("DELETE FROM `cms_album_downloads` WHERE `user_id` = '" . $user['id'] . "'");
+            mysql_query("DELETE FROM `cms_album_views` WHERE `user_id` = '" . $user['id'] . "'");
+            mysql_query("DELETE FROM `cms_album_votes` WHERE `user_id` = '" . $user['id'] . "'");
             // Удаляем почту
-            //TODO: Дописать удаление прикрепленных файлов, если были
             mysql_query("DELETE FROM `privat` WHERE `user` = '" . $user['name'] . "'");
             mysql_query("DELETE FROM `privat` WHERE `author` = '" . $user['name'] . "' AND `type` = 'out' AND `chit` = 'no'");
-            mysql_query("OPTIMIZE TABLE `privat`");
             // Удаляем карму
             mysql_query("DELETE FROM `karma_users` WHERE `karma_user` = '" . $user['id'] . "'");
             // Удаляем комментарии
-            if ($comm_count && isset($_POST['comments'])) {
+            if (isset($_POST['comments'])) {
                 if ($comm_gal) {
                     // Удаляем из Галреи
                     mysql_query("DELETE FROM `gallery` WHERE `avtor` = '" . $user['name'] . "' AND `type` = 'km'");
-                    mysql_query("OPTIMIZE TABLE `gallery`");
                 }
                 if ($comm_lib) {
                     // Удаляем из Библиотеки
                     mysql_query("DELETE FROM `lib` WHERE `avtor` = '" . $user['name'] . "' AND `type` = 'komm'");
-                    mysql_query("OPTIMIZE TABLE `lib`");
                 }
                 if ($comm_dl) {
                     // Удаляем из Загрузок
                     mysql_query("DELETE FROM `download` WHERE `avtor` = '" . $user['name'] . "' AND `type` = 'komm'");
-                    mysql_query("OPTIMIZE TABLE `download`");
+                }
+                if ($comm_gb) {
+                    // Удаляем комментарии из личных гостевых
+                    mysql_query("DELETE FROM `cms_users_guestbook` WHERE `user_id` = '" . $user['id'] . "'");
+                }
+                if ($comm_al) {
+                    // Удаляем комментарии из личных фотоальбомов
+                    mysql_query("DELETE FROM `cms_album_comments` WHERE `user_id` = '" . $user['id'] . "'");
                 }
             }
             // Удаляем посты в Гостевой
             if ($guest_count && isset($_POST['guest'])) {
                 mysql_query("DELETE FROM `guest` WHERE `user_id` = '" . $user['id'] . "'");
-                mysql_query("OPTIMIZE TABLE `guest`");
             }
             // Скрываем темы на форуме
             if ($forumt_count && isset($_POST['forumt'])) {
@@ -111,15 +124,16 @@ if (!$error) {
             if (isset($_POST['forump'])) {
                 mysql_query("UPDATE `forum` SET `close` = '1', `close_who` = '$login' WHERE `type` = 'm' AND `user_id` = '" . $user['id'] . "'");
             }
+            // Удаляем метки прочтения на Форуме
+            mysql_query("DELETE FROM `cms_forum_rdm` WHERE `user_id` = '" . $user['id'] . "'");
             // Удаляем историю нарушений
             mysql_query("DELETE FROM `cms_ban_users` WHERE `user_id` = '" . $user['id'] . "'");
-            mysql_query("OPTIMIZE TABLE `cms_ban_users`");
-            // Удаляем пользователя
-            mysql_query("DELETE FROM `users` WHERE `id` = '" . $user['id'] . "'");
-            mysql_query("OPTIMIZE TABLE `users`");
             // Удаляем историю IP
             mysql_query("DELETE FROM `cms_users_iphistory` WHERE `user_id` = '" . $user['id'] . "'");
-            mysql_query("OPTIMIZE TABLE `cms_users_iphistory`");
+            // Удаляем пользователя
+            mysql_query("DELETE FROM `users` WHERE `id` = '" . $user['id'] . "'");
+            // Оптимизируем таблицы
+            mysql_query("OPTIMIZE TABLE `cms_users_iphistory` , `cms_ban_users` , `guest`, `cms_album_comments`, `cms_users_guestbook`, `karma_users`, `cms_album_votes`, `cms_album_views`, `cms_album_downloads`, `cms_album_cat`, `cms_album_files`, `cms_forum_rdm`");
             echo '<div class="rmenu"><p><h3>' . $lng['user_deleted'] . '</h3></p></div>';
             break;
 
@@ -128,8 +142,6 @@ if (!$error) {
             // Форма параметров удаления                              //
             ////////////////////////////////////////////////////////////
             echo '<form action="index.php?act=usr_del&amp;mod=del&amp;id=' . $user['id'] . '" method="post"><div class="menu"><p><h3>' . $lng['user_del_activity'] . '</h3>';
-            if ($album)
-                echo '<div><input type="checkbox" value="1" name="gallery" checked="checked" />&#160;' . $lng['gallery'] . ' <span class="red">(' . $images_count . ')</span></div>';
             if ($comm_count)
                 echo '<div><input type="checkbox" value="1" name="comments" checked="checked" />&#160;' . $lng['comments'] . ' <span class="red">(' . $comm_count . ')</span></div>';
             if ($guest_count)
