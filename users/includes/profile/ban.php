079f28236941c88fe85056834fbe1389bbde7264
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2011-03-29 12:12:52 +0000

    [dev]   Исправлена ссылка на Бан-панель

--- users/includes/profile/ban.php
+++ users/includes/profile/ban.php
@@ -268,7 +268,7 @@ switch ($mod) {
         // Меню
         $menu = array ();
         if ($rights >= 6)
-            $menu[] = '<a href="../' . $set['admp'] . '/index.php?act=usr_ban">' . $lng_ban['ban_panel'] . '</a>';
+            $menu[] = '<a href="../' . $set['admp'] . '/index.php?act=ban_panel">' . $lng_ban['ban_panel'] . '</a>';
         if ($rights == 9)
             $menu[] = '<a href="profile.php?act=ban&amp;mod=delhist&amp;user=' . $user['id'] . '">' . $lng_ban['clear_history'] . '</a>';
         if (!empty($menu))
