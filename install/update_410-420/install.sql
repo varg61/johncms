e33ac103efc67697ff77110a4f74a73f7ad2ad31
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2011-04-23 10:11:59 +0000

    [dev]   Исправлены пакеты обновлений

--- install/update_410-420/install.sql
+++ install/update_410-420/install.sql
@@ -17,3 +17,6 @@ ALTER TABLE `users` DROP `alls`;
 ALTER TABLE `users` DROP `balans`;
 ALTER TABLE `users` DROP `set_chat`;
 ALTER TABLE `users` DROP `kod`;
+
+DELETE FROM `cms_settings` WHERE `key` = 'lng_id' LIMIT 1;
+UPDATE `cms_settings` SET `key` = 'lng' WHERE `key` = 'lng_iso' LIMIT 1;
