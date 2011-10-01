1c41bfd94f2e8bc0f89ba73202a778c21217e760
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2011-10-01 17:10:59 +0000

    [dev]   Новая функция проверки данных формы привата

--- incfiles/classes/pm.php
+++ incfiles/classes/pm.php
@@ -19,44 +19,7 @@ class pm
     function __construct($arg = array())
     {
         $this->user_id = isset($arg['user_id']) ? intval($arg['user_id']) : core::$user_id;
-        $this->max_recipients = 3; //TODO: Задать из настроек
-    }
-
-    /*
-    -----------------------------------------------------------------
-    Форма для написания сообщения
-    -----------------------------------------------------------------
-    */
-    public function message_write($action = '', $vars = array(), $error = array())
-    {
-        global $lng_pm;
-        $out = '<form name="form" action="' . $action . '" method="post"><p>' .
-               '<h3>' . $lng_pm['recipients'] . '</h3>';
-        if (!empty($vars['rcp_list'])) {
-            sort($vars['rcp_list']);
-            foreach ($vars['rcp_list'] as $val) {
-                $val = htmlspecialchars(trim($val));
-                $out .= '<div><input type="checkbox" name="todel[]" value="' . $val . '"/>&nbsp;' . $val . '</div>' .
-                        '<input type="hidden" name="tolist[]" value="' . $val . '" />';
-            }
-        }
-        if (!empty($vars['error'])) foreach ($vars['error'] as $val) $out .= '<p>' . $val . '</p>';
-        $out .= (isset($error['recipient']) ? '<p class="red">' . core::$lng['error'] . ': ' . $error['recipient'] . '</p>' : '') .
-                '<input name="to" size="15" maxlength="100" ' . (count($vars['rcp_list']) >= $this->max_recipients ? 'disabled="disabled"' : '') . (isset($error['recipient']) ? ' style="background-color: #FFCCCC"' : '') . '/>' .
-                '<input type="submit" name="add" value="+/-" /><br />' .
-                '<small><span' . (count($vars['rcp_list']) >= $this->max_recipients ? ' class="red"' : '') . '>Макс. число получателей: ' . $this->max_recipients . '</span></small>' .
-                '</p><p><h3>' . $lng_pm['subject'] . '</h3>' .
-                '<input name="subject" maxlength="200" value="' . htmlentities($vars['subject'], ENT_QUOTES, 'UTF-8') . '"/>' .
-                '</p><p><h3>' . core::$lng['message'] . '</h3>' .
-                (core::$is_mobile ? '' : bbcode::auto_bb('form', 'message')) .
-                (isset($error['body']) ? '<span class="red">' . core::$lng['error'] . ': ' . $error['body'] . '</span><br />' : '') .
-                '<textarea rows="' . core::$user_set['field_h'] . '" name="message"' . (isset($error['body']) ? ' style="background-color: #FFCCCC"' : '') . '>' . htmlentities($vars['message'], ENT_QUOTES, 'UTF-8') . '</textarea>' .
-                '</p><p><input type="checkbox" name="attach" value="1" />&#160;' . core::$lng['add_file'] . '<br />' .
-                (core::$user_set['translit'] ? '<input type="checkbox" name="translit" value="1" />&nbsp;' . core::$lng['translit'] . '<br />' : '') .
-                '<input type="checkbox" name="draft" value="1" /> <b>Черновик</b></p>' .
-                '<p><input type="submit" name="submit" value="' . core::$lng['sent'] . '"/></p>' .
-                '</form>';
-        return $out;
+        $this->max_recipients = 2; //TODO: Задать из настроек
     }
 
     /*
@@ -66,29 +29,18 @@ class pm
     */
     public function sent_message($data)
     {
+        global $lng_pm;
         $error = false;
-        $to = array();
+
         // Проверяем наличие получателей
-        if (empty($data['rcp_list']) && !$data['draft']) $error['recipient'] = 'Нет получателей';
+        if (empty($data['rcp_list']) && !$data['draft']) $error['recipient'] = $lng_pm['error_recipients'];
 
         // Проверяем наличие сообщения
-        if (mb_strlen($data['message']) < 4) $error['body'] = 'Слишком короткое сообщение';
+        if (mb_strlen($data['message']) < 4) $error['body'] = $lng_pm['error_short_message'];
 
         //TODO: Проверяем на повтор сообщений
 
         //TODO: Проверяем на флуд
-
-        // Проверяем наличие получателей
-        foreach ($data['rcp_list'] as $recipient) {
-            if (($user = $this->get_user($recipient)) !== false) {
-                // Если пользователь есть
-                $to[] = $user['name'];
-            } else {
-                // Если пользователя нет
-                $error = true;
-                break;
-            }
-        }
 
         //TODO: Проверяем на игнор у получателя
 
@@ -108,48 +60,46 @@ class pm
 
     /*
     -----------------------------------------------------------------
-    Получение данных формы отправки PM сообщения
-    -----------------------------------------------------------------
-    Возвращаемые данные:
-    $out['error'] - (ARRAY) сообщения об ошибках
-    $out['rcp_list'] - (ARRAY) список получателей
-
-    $out['subject'] - (TEXT) тема сообщения
-    $out['message'] - (TEXT) текст сообщения
-
-    $out['attach'] - (BOOLEAN) прикрепить файл
-    $out['draft'] - (BOOLEAN) отправить в черновики
-    $out['translit'] - (BOOLEAN) транслитерация
+    Получение данных формы
     -----------------------------------------------------------------
     */
     public function get_vars()
     {
-        $recipient = isset($_POST['to']) ? trim($_POST['to']) : '';
-        $out['rcp_list'] = isset($_POST['tolist']) ? $_POST['tolist'] : array();
-        $rcp_del = isset($_POST['todel']) ? $_POST['todel'] : array();
+        // Формируем список получателей
+        $rcp_string = isset($_POST['to']) ? trim($_POST['to']) : '';
+        $rcp_list = isset($_POST['tolist']) ? $_POST['tolist'] : array();
+        $rcp_array = !empty($rcp_string) ? explode(',', $rcp_string) : array();
+        $recipients = array_merge($rcp_list, $rcp_array);
+        foreach ($recipients as $key => $val) $recipients[$key] = trim($val);
+
+        // Обрабатываем запросы на удаление из списка получателей
+        $rcp_del = isset($_POST['todel']) && is_array($_POST['todel']) ? $_POST['todel'] : array();
+        $recipients = array_diff($recipients, $rcp_del);
+
+        // Формируем фозвращаемые методом данные
+        $out['rcp_list'] = array();
         $out['subject'] = isset($_POST['subject']) ? trim($_POST['subject']) : '';
         $out['message'] = isset($_POST['message']) ? trim($_POST['message']) : '';
         $out['attach'] = isset($_POST['attach']);
         $out['translit'] = isset($_POST['translit']);
         $out['draft'] = isset($_POST['draft']);
-        $count_rcp = count($out['rcp_list']);
-        if (!empty($recipient)) {
-            $array = explode(',', $recipient);
-            foreach ($array as $val) {
-                $val = trim($val);
-                if ($count_rcp < $this->max_recipients && mb_strlen($val) > 1 && mb_strlen($val) < 20 && !in_array($val, $out['rcp_list'])) {
-                    if (($user = $this->get_user($val)) !== false) $out['rcp_list'][] = $user['name'];
-                    else $out['error'][] = '&#160;<b style="text-decoration: line-through;">' . $val . '</b> <small>' . core::$lng['error_user_not_exist'] . '</small>';
+
+        // Проверяем список получателей
+        if (!empty($recipients)) {
+            $count = 1;
+            foreach ($recipients as $val) {
+                if ($count > $this->max_recipients) break;
+                if (($user = $this->get_user($val)) !== false) {
+                    $out['rcp_list'][] = $user['name'];
+                } else {
+                    $out['error'][] = '&#160;<b style="text-decoration: line-through;">' . $val . '</b> <small>' . core::$lng['error_user_not_exist'] . '</small>';
+                    continue;
                 }
-                ++$count_rcp;
+                ++$count;
             }
         }
-        if (!empty($rcp_del) && is_array($rcp_del)) {
-            foreach ($rcp_del as $val) {
-                $key = array_search(trim($val), $out['rcp_list']);
-                unset($out['rcp_list'][$key]);
-            }
-        }
+
+        // Возвращаем данные
         return $out;
     }
 
@@ -160,11 +110,9 @@ class pm
     */
     private function get_user($name = '')
     {
+        if(mb_strlen($name) < 2 || mb_strlen($name) > 20) return false;
         $req = mysql_query("SELECT `id`, `name` FROM `users` WHERE `name` = '" . mysql_real_escape_string($name) . "' LIMIT 1");
-        if (mysql_num_rows($req)) {
-            return mysql_fetch_assoc($req);
-        } else {
-            return false;
-        }
+        if (mysql_num_rows($req)) return mysql_fetch_assoc($req);
+        else return false;
     }
 }
--- users/includes/pm/write.php
+++ users/includes/pm/write.php
@@ -11,6 +11,7 @@
 
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
+$headmod = 'pm_write';
 require('../incfiles/head.php');
 
 /*
@@ -18,14 +19,44 @@ require('../incfiles/head.php');
 Отправка сообщения
 -----------------------------------------------------------------
 */
-$data = $pm->get_vars();
 $error = false;
+$data = $pm->get_vars();
 if (isset($_POST['submit']) && !isset($data['error']) && ($error = $pm->sent_message($data)) === false) {
     echo '<p>Отправлено</p>';
 } else {
     // Выводим форму для написания письма
-    echo '<div class="phdr"><b><a href="pm.php">' . $lng_pm['my_mail'] . '</a></b> | Написать письмо</div>' .
-         '<div class="gmenu">' . $pm->message_write('pm.php?act=write', $data, $error) . '</div>' .
-         '<div class="phdr"><a href="../pages/faq.php?act=trans">Транслит</a> | <a href="../pages/faq.php?act=smileys">Смайлы</a></div>' .
-         '<p><a href="' . htmlspecialchars($_SERVER['HTTP_REFERER']) . '">Назад</a></p>';
+    echo '<div class="phdr"><b><a href="pm.php">' . $lng_pm['my_mail'] . '</a></b> | ' . $lng_pm['write_message'] . '</div>' .
+         '<div class="gmenu">' .
+         '<form name="form" action="pm.php?act=write" method="post"><p>' .
+         '<h3>' . $lng_pm['recipients'] . '</h3>';
+    if (!empty($data['rcp_list'])) {
+        //sort($data['rcp_list']);
+        foreach ($data['rcp_list'] as $val) {
+            $recipient = htmlspecialchars($val);
+            echo '<div><input type="checkbox" name="todel[]" value="' . $val . '"/>&nbsp;' . $val . '</div>' .
+                 '<input type="hidden" name="tolist[]" value="' . $val . '" />';
+        }
+    }
+    if (!empty($data['error'])) foreach ($data['error'] as $val) echo '<div>' . $val . '</div>';
+    echo (isset($error['recipient']) ? '<p class="red">' . core::$lng['error'] . ': ' . $error['recipient'] . '</p>' : '') .
+         '<input name="to" size="15" maxlength="100" ' . (count($data['rcp_list']) >= $max_rcp ? 'disabled="disabled"' : '') . (isset($error['recipient']) ? ' style="background-color: #FFCCCC"' : '') . '/>' .
+         '<input type="submit" name="add" value="+/-" /><br />' .
+         '<small><span' . (count($data['rcp_list']) >= $max_rcp ? ' class="red"' : '') . '>' . $lng_pm['max_recipients'] . ': ' . $max_rcp . '</span></small>' .
+         '</p><p>' .
+         '<h3>' . $lng_pm['subject'] . '</h3>' .
+         '<input name="subject" maxlength="200" value="' . htmlentities($data['subject'], ENT_QUOTES, 'UTF-8') . '"/>' .
+         '</p><p>' .
+         '<h3>' . core::$lng['message'] . '</h3>' .
+         (core::$is_mobile ? '' : bbcode::auto_bb('form', 'message')) .
+         (isset($error['body']) ? '<span class="red">' . core::$lng['error'] . ': ' . $error['body'] . '</span><br />' : '') .
+         '<textarea rows="' . core::$user_set['field_h'] . '" name="message"' . (isset($error['body']) ? ' style="background-color: #FFCCCC"' : '') . '>' . htmlentities($data['message'], ENT_QUOTES, 'UTF-8') . '</textarea>' .
+         '</p><p>' .
+         '<input type="checkbox" name="attach" value="1" />&#160;' . core::$lng['add_file'] . '<br />' .
+         (core::$user_set['translit'] ? '<input type="checkbox" name="translit" value="1" />&nbsp;' . core::$lng['translit'] . '<br />' : '') .
+         '<input type="checkbox" name="draft" value="1" /> <b>' . $lng_pm['draft'] . '</b>' .
+         '</p><p>' .
+         '<input type="submit" name="submit" value="' . core::$lng['sent'] . '"/>' .
+         '</p></form></div>' .
+         '<div class="phdr">' . (core::$user_set['translit'] ? '<a href="../pages/faq.php?act=trans">' . core::$lng['translit'] . '</a> | ' : '') . '<a href="../pages/faq.php?act=smileys">' . core::$lng['smileys'] . '</a></div>' .
+         '<p><a href="' . htmlspecialchars($_SERVER['HTTP_REFERER']) . '">' . core::$lng['back'] . '</a></p>';
 }
