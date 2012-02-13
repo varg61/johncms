1b7773826cbe90d9b5b72ccc3e44c13404f9959d
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-02-13 19:11:30 +0000

    [dev]   Личный кабинет переделан под шаблон

--- modules/users/cabinet/index.php
+++ modules/users/cabinet/index.php
@@ -23,29 +23,7 @@ if (!Vars::$USER_ID) {
     exit;
 }
 
-$lng_profile = Vars::loadLanguage(1);
-
-$total_photo = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_files` WHERE `user_id` = " . Vars::$USER_ID), 0);
-echo '<div class="phdr"><b>' . $lng_profile['my_office'] . '</b></div>' .
-    '<div class="list2"><p>' .
-    '<div>' . Functions::getImage('contacts.png') . '&#160;<a href="profile.php">' . $lng_profile['my_profile'] . '</a></div>' .
-    '<div>' . Functions::getImage('rating.png') . '&#160;<a href="profile.php?act=stat">' . Vars::$LNG['statistics'] . '</a></div>' .
-    '</p><p>' .
-    '<div>' . Functions::getImage('album_4.png') . '&#160;<a href="album.php?act=list">' . Vars::$LNG['photo_album'] . '</a>&#160;(' . $total_photo . ')</div>' .
-    '<div>' . Functions::getImage('comments.png') . '&#160;<a href="profile.php?act=guestbook">' . Vars::$LNG['guestbook'] . '</a>&#160;()</div>' .
-    (Vars::$USER_RIGHTS >= 1 ? '</p><p><div>' . Functions::getImage('blocked.png') . '&#160;<a href="../guestbook/index.php?act=ga&amp;do=set">' . Vars::$LNG['admin_club'] . '</a> (<span class="red">()</span>)</div>' : '') .
-    '</p></div><div class="menu"><p>' .
-    '<h3>' . Functions::getImage('mail-inbox.png') . '&#160;' . $lng_profile['my_mail'] . '</h3><ul>' .
-    '<li><a href="">' . Vars::$LNG['mail_new'] . '</a>&#160;(x)</li>' .
-    '<li><a href="pm.php">' . $lng_profile['all_mail'] . '</a></li>' .
-    (!isset(Vars::$USER_BAN['1']) && !isset(Vars::$USER_BAN['3']) ? '<p><form action="pm.php?act=write" method="post"><input type="submit" value=" ' . Vars::$LNG['write'] . ' " /></form></p>' : '') .
-    '</ul><h3>' . Functions::getImage('contacts.png') . '&#160;' . Vars::$LNG['contacts'] . '</h3><ul>' .
-    '<li><a href="cont.php">' . Vars::$LNG['contacts'] . '</a>&#160;()</li>' .
-    '<li><a href="ignor.php">' . Vars::$LNG['blocking'] . '</a>&#160;()</li>' .
-    '</ul></p></div>' .
-    '<div class="bmenu"><p><h3>' . Functions::getImage('settings.png') . '&#160;' . $lng_profile['my_settings'] . '</h3><ul>' .
-    '<li><a href="profile.php?act=settings">' . Vars::$LNG['system_settings'] . '</a></li>' .
-    '<li><a href="profile.php?act=edit">' . $lng_profile['profile_edit'] . '</a></li>' .
-    '<li><a href="profile.php?act=password">' . Vars::$LNG['change_password'] . '</a></li>' .
-    (Vars::$USER_RIGHTS >= 1 ? '<li><span class="red"><a href="' . Vars::$HOME_URL . '/' . Vars::$SYSTEM_SET['admp'] . '/index.php"><b>' . Vars::$LNG['admin_panel'] . '</b></a></span></li>' : '') .
-    '</ul></p></div>';
+$tpl = Template::getInstance();
+$tpl->lng = Vars::loadLanguage(1);
+$tpl->total_photo = mysql_result(mysql_query("SELECT COUNT(*) FROM `cms_album_files` WHERE `user_id` = " . Vars::$USER_ID), 0);
+$tpl->contents = $tpl->includeTpl('cabinet');
