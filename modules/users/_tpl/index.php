f95108188a021998593804dea9fe37bf3abee40e
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-03-12 21:57:55 +0000

    [dev]   Идет доработка модулей под новую функцию обработки языков

--- modules/users/_tpl/index.php
+++ modules/users/_tpl/index.php
@@ -1,29 +1,33 @@
-<div class="phdr"><b><?= Vars::$LNG['community'] ?></b></div>
+<div class="phdr">
+    <b><?= lng('community') ?></b>
+</div>
 <div class="gmenu">
     <form action="<?= Vars::$URI ?>/search" method="post">
         <p>
-        <h3><?= Vars::$LNG['search'] ?></h3>
+        <h3><?= lng('search') ?></h3>
         <input type="text" name="search"/>
-        <input type="submit" value="<?= Vars::$LNG['search'] ?>" name="submit"/><br/>
-        <small><?= Vars::$LNG['search_nick_help'] ?></small>
+        <input type="submit" value="<?= lng('search') ?>" name="submit"/><br/>
+        <small><?= lng('search_nick_help') ?></small>
         </p>
     </form>
 </div>
 <div class="menu">
     <p>
-    <h3><?= Functions::getImage('users.png') . '&#160;' . Vars::$LNG['community'] ?></h3>
+    <h3><?= Functions::getImage('users.png') . '&#160;' . lng('community') ?></h3>
     <ul>
-        <li><a href="<?= Vars::$URI ?>/list"><?= Vars::$LNG['users'] ?></a> (<?= $this->count->users ?>)</li>
-        <li><a href="<?= Vars::$URI ?>/list?act=adm"><?= Vars::$LNG['administration'] ?></a></li>
+        <li><a href="<?= Vars::$URI ?>/list"><?= lng('users') ?></a> (<?= $this->count->users ?>)</li>
+        <li><a href="<?= Vars::$URI ?>/list?act=adm"><?= lng('administration') ?></a></li>
     </ul>
     </p>
     <p>
-    <h3><?= Functions::getImage('rating.png') . '&#160;' . Vars::$LNG['users_top'] ?></h3>
+    <h3><?= Functions::getImage('rating.png') . '&#160;' . lng('users_top') ?></h3>
     <ul>
-        <li><a href="<?= Vars::$URI ?>/top"><?= Vars::$LNG['forum'] ?></a></li>
-        <li><a href="<?= Vars::$URI ?>/top?act=comm"><?= Vars::$LNG['comments'] ?></a></li>
-        <li><a href="<?= Vars::$URI ?>/top?act=karma"><?= Vars::$LNG['karma'] ?></a></li>
+        <li><a href="<?= Vars::$URI ?>/top"><?= lng('forum') ?></a></li>
+        <li><a href="<?= Vars::$URI ?>/top?act=comm"><?= lng('comments') ?></a></li>
+        <li><a href="<?= Vars::$URI ?>/top?act=karma"><?= lng('karma') ?></a></li>
     </ul>
     </p>
 </div>
-<div class="phdr"><a href="index.php"><?= Vars::$LNG['back'] ?></a></div>
+<div class="phdr">
+    <a href="index.php"><?= lng('back') ?></a>
+</div>
