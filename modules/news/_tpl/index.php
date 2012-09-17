ee4eafa5995d94e3e67c12c90780ad5cae0bc8ba
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-09-17 12:02:25 +0000

    [dev]   Продолжена работа с модулем новостей

--- modules/news/_tpl/index.php
+++ modules/news/_tpl/index.php
@@ -2,25 +2,29 @@
     <li><h1><?= lng('site_news') ?></h1></li>
 </ul>
 <?php if (isset($this->news)): ?>
-<?php foreach ($this->news as $key => $val): ?>
+    <?php foreach ($this->news as $key => $val): ?>
     <div class="<?= $key % 2 ? 'block-odd' : 'block-even' ?>">
         <div class="block-hdr"><?= $val['name'] ?></div>
         <div class="block-text"><?= $val['text'] ?></div>
+        <div class="list-tools">
+            <a class="btn btn-mini" href="<?= Vars::$URI ?>"><?= lng('edit') ?></a>
+            <a class="btn btn-mini" href="<?= Vars::$URI ?>"><?= lng('delete') ?></a>
+        </div>
     </div>
     <?php endforeach ?>
-<ul class="nav">
-    <li><h1><?= lng('total') ?>:&#160;<?= $this->total ?></h1></li>
-</ul>
-<?php if (isset($this->pagination)): ?>
-    <div class="align-center"><?= $this->pagination ?></div>
-    <?php endif ?>
+    <ul class="nav">
+        <li><h1><?= lng('total') ?>:&#160;<?= $this->total ?></h1></li>
+    </ul>
+    <?php if (isset($this->pagination)): ?>
+        <div class="align-center"><?= $this->pagination ?></div>
+        <?php endif ?>
 <?php else: ?>
-<div class="form-container">
-    <div class="form-block align-center">
-        <?= lng('list_empty') ?>
+    <div class="form-container">
+        <div class="form-block align-center">
+            <?= lng('list_empty') ?>
+        </div>
     </div>
-</div>
 <?php endif ?>
 <?php if(Vars::$USER_RIGHTS >= 7): ?>
-<div><a class="btn" href="<?= Vars::$URI ?>?act=add">Добавить новость</a></div>
+    <div class="btn-panel"><a class="btn" href="<?= Vars::$URI ?>?act=add">Добавить новость</a></div>
 <?php endif ?>
