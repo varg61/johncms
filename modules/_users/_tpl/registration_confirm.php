460ae71a9afc895ff4edfaee02081b694ce89cde
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-09-21 11:35:38 +0000

    [dev]   Доработана страница подтверждения регистрации [dev]   Исправлена ссылка на профили пользователей

--- modules/_users/_tpl/registration_confirm.php
+++ modules/_users/_tpl/registration_confirm.php
@@ -1,39 +1,39 @@
-<div class="phdr">
-    <b><?= lng('registration') ?></b>
-</div>
-<div class="user">
-    <p><?= Functions::displayUser(Vars::$USER_DATA) ?></p>
-    <p>
-    <table>
-        <tr>
-            <td align="right" valign="top"><?= lng('password') ?>:</td>
-            <td>
-                <?php if (isset($_GET['pass'])) : ?>
-                <strong><?= htmlspecialchars($_SESSION['password']) ?></strong>
-                <?php else : ?>
-                <a href="<?= Vars::$URI ?>?pass<?= (isset($_GET['auto']) ? '&amp;auto' : '') ?>"><?= lng('show') ?></a>
-                <?php endif; ?>
-            </td>
-        </tr>
-        <tr>
-            <td><?= lng('autologin') ?>:</td>
-            <td>
-                <?php if (isset($_GET['auto'])) : ?>
-                <input type="text" value="<?= Vars::$HOME_URL . '/login?id=' . $_SESSION['uid'] . '&amp;token=' . htmlspecialchars($_SESSION['token']) ?>"/>
-                <?php else : ?>
-                <a href="<?= Vars::$URI ?>?auto<?= (isset($_GET['pass']) ? '&amp;pass' : '') ?>"><?= lng('show') ?></a>
-                <?php endif; ?>
-            </td>
-        </tr>
-    </table>
-    </p>
-</div>
-<div class="gmenu">
-    <h3><?= lng('thanks_for_registration') ?>!</h3>
-    <?php if (Vars::$USER_SYS['reg_moderation'] == 2) : ?>
-    <p style="font-size: x-small"><?= lng('moderation_warning') ?></p>
-    <?php endif; ?>
-</div>
-<div class="phdr">
-    <a href="<?= Vars::$HOME_URL ?>"><?= lng('enter_on_site') ?></a>
+<ul class="nav">
+    <li><h1><?= lng('registration') ?></h1></li>
+</ul>
+<div class="form-container">
+    <div class="form-block">
+        <?= Functions::displayUser(Vars::$USER_DATA, array('iphide' => 1,)) ?>
+    </div>
+    <div class="form-block">
+        <span class="info-message"><?= lng('thanks_for_registration') ?>!</span>
+        <?php if (Vars::$USER_SYS['reg_moderation']): ?>
+        <br/><br/><span class="input-help"><?= lng('moderation_warning') ?></span>
+        <?php endif ?>
+        <br/><br/>
+        <table>
+            <tr>
+                <td align="right" valign="top"><?= lng('password') ?>:</td>
+                <td>
+                    <?php if (isset($_GET['pass'])): ?>
+                    <strong><?= htmlspecialchars($_SESSION['password']) ?></strong>
+                    <?php else: ?>
+                    <a href="<?= Vars::$URI ?>?pass<?= (isset($_GET['auto']) ? '&amp;auto' : '') ?>"><?= lng('show') ?></a>
+                    <?php endif ?>
+                </td>
+            </tr>
+            <tr>
+                <td><?= lng('autologin') ?>:</td>
+                <td>
+                    <?php if (isset($_GET['auto'])): ?>
+                    <input type="text" value="<?= Vars::$HOME_URL . '/login?id=' . $_SESSION['uid'] . '&amp;token=' . htmlspecialchars($_SESSION['token']) ?>"/>
+                    <?php else: ?>
+                    <a href="<?= Vars::$URI ?>?auto<?= (isset($_GET['pass']) ? '&amp;pass' : '') ?>"><?= lng('show') ?></a>
+                    <?php endif ?>
+                </td>
+            </tr>
+        </table>
+
+        <br/><a class="btn btn-primary btn-large" href="<?= Vars::$HOME_URL ?>"><?= lng('enter_on_site') ?></a>
+    </div>
 </div>
