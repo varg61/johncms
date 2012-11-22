879ea1e1466efa2d74464d1bf2d811add5c99be9
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-11-22 10:43:56 +0000

    [dev]   Доработан внешний вид Гостевой

--- modules/guestbook/_tpl/index.php
+++ modules/guestbook/_tpl/index.php
@@ -15,27 +15,27 @@
     <?php if(!Vars::$USER_ID): ?>
     <?= lng('name') ?> (max 25):<br/><input type="text" name="name" maxlength="25"/><br/>
     <?php endif ?>
-    <b><?= lng('message') ?></b> <small>(max 5000)</small>:<br/>
     <?= (Vars::$USER_ID && !Vars::$IS_MOBILE ? TextParser::autoBB('form', 'msg') : '') ?>
     <textarea rows="<?= Vars::$USER_SET['field_h'] ?>" name="msg"></textarea><br/>
     <?php if (!Vars::$USER_ID): ?>
     <?= Captcha::display(1) ?><br/>
     <?php endif ?>
-    <input class="btn btn-primary" type="submit" name="submit" value="<?= lng('sent') ?>"/>
+    <input class="btn btn-primary" type="submit" name="submit" value="<?= lng('write') ?>"/>
     <input type="hidden" name="form_token" value="<?= $this->form_token ?>"/>
 </form>
 </div>
 
 <ul class="nav"><li><h2><?= lng('comments') ?></h2></li></ul>
 <?php if (isset($this->list)): ?>
-<?php foreach ($this->list as $key => $val): ?>
-    <div class="<?= $key % 2 ? 'block-odd' : 'block-even' ?>">
+    <?php foreach ($this->list as $key => $val): ?>
+        <div class="<?= $key % 2 ? 'block-odd' : 'block-even' ?>">
         <?= $val ?>
-    </div>
+        </div>
     <?php endforeach ?>
+    <ul class="nav"><li><h2><?= lng('total') . ': ' . $this->total ?></h2></li></ul>
     <?php if (Vars::$USER_RIGHTS >= 7): ?>
     <a href="<?= Vars::$URI . '?act=clean' . ($this->mod ? '&amp;mod=adm' : '') ?>"><?= lng('clear') ?></a>
     <?php endif ?>
 <?php else: ?>
-<div class="block-even align-center"><br/><?= lng('guestbook_empty') ?><br/><br/></div>
+    <div class="block-even align-center"><br/><?= lng('guestbook_empty') ?><br/><br/></div>
 <?php endif ?>
