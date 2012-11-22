7b9f0e34bf68112a0644bbafbe6a6652ad8ff7e3
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-11-22 12:32:33 +0000

    [dev]   Добавлен шаблон для системных настроек Админ панели

--- modules/_admin/_tpl/settings.php
+++ modules/_admin/_tpl/settings.php
@@ -1,40 +1,39 @@
-<div class="phdr">
-    <a href="<?= Vars::$MODULE_URI ?>"><b><?= lng('admin_panel') ?></b></a> | <?= lng('system_settings') ?>
+<ul class="nav">
+    <li><h1 class="section-warning"><?= lng('system_settings') ?></h1></li>
+</ul>
+<div class="form-container">
+    <?php if (isset($this->save)) : ?>
+    <div class="form-block"><?= lng('settings_saved') ?></div>
+    <?php endif ?>
+    <?php if (isset($this->reset)) : ?>
+    <div class="form-block"><?= lng('settings_default') ?></div>
+    <?php endif ?>
+
+    <form action="<?= Vars::$URI ?>" method="post">
+        <div class="form-block">
+            <label for="timeshift"><?= lng('time_shift') ?></label><br/>
+            <input id="timeshift" class="small" type="text" name="timeshift" size="2" maxlength="3" value="<?= Vars::$SYSTEM_SET['timeshift'] ?>"/><span class="input-help">&#160;+-12 <?= lng('hours') ?></span><br/>
+            <label for="timeshift" class="small">
+                <span class="badge badge-green"><?= date("H:i", time() + Vars::$SYSTEM_SET['timeshift'] * 3600) ?></span> <?= lng('system_time') ?><br/>
+                <span class="badge badge-red"><?= date("H:i") ?></span> <?= lng('server_time') ?>
+            </label>
+            <br/><br/>
+            <label for="copyright"><?= lng('site_copyright') ?></label><br/>
+            <input id="copyright" type="text" name="copyright" value="<?= Validate::filterString(Vars::$SYSTEM_SET['copyright']) ?>"/><br/>
+            <label for="email"><?= lng('site_email') ?></label><br/>
+            <input id="email" type="text" name="email" value="<?= Validate::filterString(Vars::$SYSTEM_SET['email']) ?>"/><br/>
+            <label for="filesize"><?= lng('file_maxsize') ?></label><br/>
+            <input id="filesize" class="small" type="text" name="filesize" value="<?= intval(Vars::$SYSTEM_SET['flsz']) ?>"/><span class="input-help">&#160;kB</span><br/>
+            <label class="small"><input name="gzip" type="checkbox" value="1" <?= (Vars::$SYSTEM_SET['gzip'] ? 'checked="checked"' : '') ?>/>&#160;<?= lng('gzip_compress') ?></label>
+            <br/><br/>
+            <label for="keywords">META Keywords</label><br/>
+            <textarea id="keywords" rows="<?= Vars::$USER_SET['field_h'] ?>" name="keywords"><?= Validate::filterString(Vars::$SYSTEM_SET['meta_key']) ?></textarea><br/>
+            <label for="description">META Description</label><br/>
+            <textarea id="description" rows="<?= Vars::$USER_SET['field_h'] ?>" name="description"><?= Validate::filterString(Vars::$SYSTEM_SET['meta_desc']) ?></textarea>
+            <br/><br/>
+            <input class="btn btn-primary btn-large" type="submit" name="submit" value=" <?= lng('save') ?> "/>
+            <a class="btn" href="<?= Vars::$MODULE_URI ?>"><?= lng('back') ?></a>
+            <input type="hidden" name="form_token" value="<?= $this->form_token ?>"/>
+        </div>
+    </form>
 </div>
-<form action="<?= Vars::$URI ?>" method="post">
-    <div class="menu">
-        <div class="formblock">
-            <label><?= lng('common_settings') ?></label><br/>
-            <?= lng('site_copyright') ?>:<br/>
-            <input type="text" name="copyright" value="<?= htmlentities(Vars::$SYSTEM_SET['copyright'], ENT_QUOTES, 'UTF-8') ?>"/><br/>
-            <?= lng('site_email') ?>:<br/>
-            <input name="madm" maxlength="50" value="<?= htmlentities(Vars::$SYSTEM_SET['email']) ?>"/><br/>
-            <?= lng('file_maxsize') ?> (kb):<br/>
-            <input type="text" name="flsz" value="<?= intval(Vars::$SYSTEM_SET['flsz']) ?>"/><br/>
-            <input name="gzip" type="checkbox" value="1" <?= (Vars::$SYSTEM_SET['gzip'] ? 'checked="checked"' : '') ?>/>&#160;<?= lng('gzip_compress') ?>
-        </div>
-        <div class="formblock">
-            <label><?= lng('clock_settings') ?></label><br/>
-            <span style="font-weight:bold; background-color:#C0FFC0"><?= date("H:i", time() + Vars::$SYSTEM_SET['timeshift'] * 3600) ?></span> <?= lng('system_time') ?><br/>
-            <span style="font-weight:bold; background-color:#FFC0C0"><?= date("H:i") ?></span> <?= lng('server_time') ?><br/>
-            <input type="text" name="timeshift" size="2" maxlength="3" value="<?= Vars::$SYSTEM_SET['timeshift'] ?>"/> <?= lng('time_shift') ?> (+-12)
-        </div>
-        <div class="formblock">
-            <label><?= lng('meta_tags') ?></label><br/>
-            <?= lng('meta_keywords') ?>:<br/>
-            <textarea rows="<?= Vars::$USER_SET['field_h'] ?>" name="meta_key"><?= Vars::$SYSTEM_SET['meta_key'] ?></textarea><br/>
-            <?= lng('meta_description') ?>:<br/>
-            <textarea rows="<?= Vars::$USER_SET['field_h'] ?>" name="meta_desc"><?= Vars::$SYSTEM_SET['meta_desc'] ?></textarea>
-        </div>
-        <div class="formblock">
-            <label><?= lng('design_template') ?></label><br/>
-        </div>
-        <div class="formblock">
-            <input type="submit" name="submit" value="<?= lng('save') ?>"/>
-        </div>
-    </div>
-</form>
-<div class="phdr">
-    &#160;
-</div>
-<p><a href="<?= Vars::$MODULE_URI ?>"><?= lng('admin_panel') ?></a></p>
