e1108ad10d327807cc0609aff3dbcd509949215e
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2012-12-21 10:51:31 +0000

    [dev]   Доработана админка Новостей

--- modules/news/_tpl/admin.php
+++ modules/news/_tpl/admin.php
@@ -2,11 +2,11 @@
     <li><h1><?= __('news_on_frontpage') ?></h1></li>
 </ul>
 <div class="form-container">
-    <?php if (isset($this->default)): ?>
-    <div class="form-block confirm"><?= __('settings_default') ?></div>
-    <?php endif ?>
-    <?php if (isset($this->saved)): ?>
+    <?php if (isset($this->save)): ?>
     <div class="form-block confirm"><?= __('settings_saved') ?></div>
+    <?php endif ?>
+    <?php if (isset($_GET['default'])): ?>
+    <div class="form-block confirm"><?= __('settings_default') ?></div>
     <?php endif ?>
     <div class="form-block">
         <?= $this->form ?>
--- modules/news/admin.php
+++ modules/news/admin.php
@@ -16,14 +16,24 @@ if (Vars::$USER_RIGHTS < 7) {
 }
 
 $tpl = Template::getInstance();
-$tpl->settings = unserialize(Vars::$SYSTEM_SET['news']);
+$settings = isset(Vars::$SYSTEM_SET['news'])
+    ? unserialize(Vars::$SYSTEM_SET['news'])
+    : array(
+        'view'     => 1,
+        'breaks'   => 1,
+        'smileys'  => 1,
+        'tags'     => 1,
+        'comments' => 1,
+        'size'     => 500,
+        'quantity' => 3,
+        'days'     => 7
+    );
 
 $form = new Form(Vars::$URI);
-
 $form
     ->addField('radio', 'view', array(
     'label'   => __('apperance'),
-    'checked' => $tpl->settings['view'],
+    'checked' => $settings['view'],
     'items'   => array(
         '1' => __('heading_and_text'),
         '2' => __('heading'),
@@ -35,37 +45,37 @@ $form
 
     ->addField('checkbox', 'breaks', array(
     'label_inline' => __('line_foldings'),
-    'checked'      => $tpl->settings['breaks']))
+    'checked'      => $settings['breaks']))
 
     ->addField('checkbox', 'smileys', array(
     'label_inline' => __('smileys'),
-    'checked'      => $tpl->settings['smileys']))
+    'checked'      => $settings['smileys']))
 
     ->addField('checkbox', 'tags', array(
     'label_inline' => __('bbcode'),
-    'checked'      => $tpl->settings['tags']))
+    'checked'      => $settings['tags']))
 
     ->addField('checkbox', 'comments', array(
     'label_inline' => __('comments'),
-    'checked'      => $tpl->settings['comments']))
+    'checked'      => $settings['comments']))
 
     ->addHtml('<br/>')
 
     ->addField('text', 'size', array(
     'label_inline' => __('text_size') . ' (100 - 5000)',
-    'value'        => $tpl->settings['size'],
+    'value'        => $settings['size'],
     'maxlength'    => '4',
     'class'        => 'small'))
 
     ->addField('text', 'quantity', array(
     'label_inline' => __('news_count') . ' (1 - 15)',
-    'value'        => $tpl->settings['quantity'],
+    'value'        => $settings['quantity'],
     'maxlength'    => '2',
     'class'        => 'mini'))
 
     ->addField('text', 'days', array(
     'label_inline' => __('news_howmanydays_display') . ' (1 - 30)',
-    'value'        => $tpl->settings['days'],
+    'value'        => $settings['days'],
     'maxlength'    => '2',
     'class'        => 'mini'))
 
@@ -75,55 +85,48 @@ $form
     'value' => __('save'),
     'class' => 'btn btn-primary btn-large'))
 
-    ->addHtml(' <a class="btn" href="' . Vars::$URI . '?reset">' . __('reset_settings') . '</a>')
+    ->addField('submit', 'reset', array(
+    'value' => __('reset_settings'),
+    'class' => 'btn'))
 
     ->addHtml('<a class="btn" href="' . Vars::$MODULE_URI . '">' . __('back') . '</a>');
 
 $tpl->form = $form->display();
 
-/*
------------------------------------------------------------------
-Настройки Новостей
------------------------------------------------------------------
-*/
-if (!isset(Vars::$SYSTEM_SET['news']) || isset($_GET['reset'])) {
-    // Задаем настройки по умолчанию
-    $tpl->settings = array(
-        'view'     => '1',
-        'size'     => '500',
-        'quantity' => '3',
-        'days'     => '7',
-        'breaks'   => '1',
-        'smileys'  => '1',
-        'tags'     => '1',
-        'comments' => '1'
-    );
-    mysql_query("INSERT INTO `cms_settings` SET
+if ($form->submit && isset($form->input['submit'])) {
+    foreach ($form->validInput as $key => $val) {
+        $settings[$key] = $val;
+    }
+
+    // Проверяем принятые данные
+    if ($settings['size'] < 100) {
+        $settings['size'] = 100;
+    } elseif ($settings['size'] > 5000) {
+        $settings['size'] = 5000;
+    }
+
+    if ($settings['quantity'] < 1) {
+        $settings['quantity'] = 1;
+    } elseif ($settings['quantity'] > 15) {
+        $settings['quantity'] = 15;
+    }
+
+    if ($settings['days'] < 1) {
+        $settings['days'] = 1;
+    } elseif ($settings['days'] > 30) {
+        $settings['days'] = 30;
+    }
+
+    mysql_query("REPLACE INTO `cms_settings` SET
         `key` = 'news',
-        `val` = '" . mysql_real_escape_string(serialize($tpl->settings)) . "'
-        ON DUPLICATE KEY UPDATE
-        `val` = '" . mysql_real_escape_string(serialize($tpl->settings)) . "'
-    ");
-    $tpl->default = 1;
-} elseif (isset($_POST['submit'])) {
-    // Принимаем настройки из формы
-    $settings['view'] = isset($_POST['view']) && $_POST['view'] >= 0 && $_POST['view'] < 4 ? intval($_POST['view']) : 1;
-    $settings['size'] = isset($_POST['size']) && $_POST['size'] >= 100 && $_POST['size'] <= 1000 ? intval($_POST['size']) : 500;
-    $settings['quantity'] = isset($_POST['quantity']) && $_POST['quantity'] > 0 && $_POST['quantity'] < 16 ? intval($_POST['quantity']) : 3;
-    $settings['days'] = isset($_POST['days']) && $_POST['days'] > 0 && $_POST['days'] < 16 ? intval($_POST['days']) : 7;
-    $settings['breaks'] = isset($_POST['breaks']);
-    $settings['smileys'] = isset($_POST['smileys']);
-    $settings['tags'] = isset($_POST['tags']);
-    $settings['comments'] = isset($_POST['comments']);
-    mysql_query("UPDATE `cms_settings` SET
         `val` = '" . mysql_real_escape_string(serialize($settings)) . "'
-        WHERE `key` = 'news'
     ");
-    $tpl->settings = $settings;
-    $tpl->saved = 1;
-} else {
-    // Получаем сохраненные настройки
-    $tpl->settings = unserialize(Vars::$SYSTEM_SET['news']);
+
+    $tpl->save = true;
+} elseif ($form->submit && isset($form->input['reset'])) {
+    @mysql_query("DELETE FROM `cms_settings` WHERE `key` = 'news'");
+    header('Location: ' . Vars::$URI . '?default');
+    exit;
 }
 
 $tpl->contents = $tpl->includeTpl('admin');
