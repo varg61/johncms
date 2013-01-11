2997227e1ac1e863e341e375ef6fab398c41e458
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2013-01-11 22:07:33 +0000

    [dev]   Класс Advt доработан под PDO

--- includes/classes/advt.php
+++ includes/classes/advt.php
@@ -15,24 +15,25 @@ class Advt extends Vars
     {
         $ads = array();
         $view = Vars::$USER_ID ? 2 : 1;
-        $layout = (empty(parent::$PLACE) && !Vars::$ACT) ? 1 : 2;
-        $req = mysql_query("SELECT * FROM `cms_ads` WHERE `to` = '0' AND (`layout` = '$layout' or `layout` = '0') AND (`view` = '$view' or `view` = '0') ORDER BY  `mesto` ASC");
-        if (mysql_num_rows($req)) {
-            while (($res = mysql_fetch_assoc($req)) !== false) {
-                $name = explode("|", $res['name']);
+        $layout = empty(static::$PLACE) && !Vars::$ACT ? 1 : 2;
+        $STH = DB::PDO()->query("SELECT * FROM `cms_ads` WHERE `to` = '0' AND (`layout` = '$layout' or `layout` = '0') AND (`view` = '$view' or `view` = '0') ORDER BY  `mesto` ASC");
+        if ($STH->rowCount()) {
+            while ($result = $STH->fetch()) {
+                $name = explode("|", $result['name']);
                 $name = htmlentities($name[mt_rand(0, (count($name) - 1))], ENT_QUOTES, 'UTF-8');
-                if (!empty($res['color'])) $name = '<span style="color:#' . $res['color'] . '">' . $name . '</span>';
+                if (!empty($result['color'])) $name = '<span style="color:#' . $result['color'] . '">' . $name . '</span>';
                 // Если было задано начертание шрифта, то применяем
-                $font = $res['bold'] ? 'font-weight: bold;' : false;
-                $font .= $res['italic'] ? ' font-style:italic;' : false;
-                $font .= $res['underline'] ? ' text-decoration:underline;' : false;
+                $font = $result['bold'] ? 'font-weight: bold;' : FALSE;
+                $font .= $result['italic'] ? ' font-style:italic;' : FALSE;
+                $font .= $result['underline'] ? ' text-decoration:underline;' : FALSE;
                 if ($font) $name = '<span style="' . $font . '">' . $name . '</span>';
                 //TODO: Переделать ссылку редиректа
-                @$ads[$res['type']] .= '<a href="' . ($res['show'] ? Validate::checkout($res['link']) : Vars::$HOME_URL . '/go.php?id=' . $res['id']) . '">' . $name . '</a><br/>';
-                if (($res['day'] != 0 && time() >= ($res['time'] + $res['day'] * 3600 * 24)) || ($res['count_link'] != 0 && $res['count'] >= $res['count_link']))
-                    mysql_query("UPDATE `cms_ads` SET `to` = '1'  WHERE `id` = '" . $res['id'] . "'");
+                @$ads[$result['type']] .= '<a href="' . ($result['show'] ? Validate::checkout($result['link']) : Vars::$HOME_URL . '/go.php?id=' . $result['id']) . '">' . $name . '</a><br/>';
+                if (($result['day'] != 0 && time() >= ($result['time'] + $result['day'] * 3600 * 24)) || ($result['count_link'] != 0 && $result['count'] >= $result['count_link']))
+                    mysql_query("UPDATE `cms_ads` SET `to` = '1'  WHERE `id` = '" . $result['id'] . "'");
             }
         }
+
         return $ads;
     }
 }
