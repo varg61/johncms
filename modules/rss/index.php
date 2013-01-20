fbb9547457487da54ec511b9cc97f4efb33c1799
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2013-01-20 19:53:55 +0000

    [dev]   RSS доработан под PDO

--- modules/rss/index.php
+++ modules/rss/index.php
@@ -12,44 +12,44 @@
 defined('_IN_JOHNCMS') or die('Error: restricted access');
 
 $tpl = Template::getInstance();
-$tpl->template = false;
+$tpl->template = FALSE;
 
 header('content-type: application/rss+xml');
 echo '<?xml version="1.0" encoding="utf-8"?>' .
-     '<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/"><channel>' .
-     '<title>' . htmlspecialchars(Vars::$SYSTEM_SET['copyright']) . ' | News</title>' .
-     '<link>' . Vars::$HOME_URL . '</link>' .
-     '<description>News</description>' .
-     '<language>ru-RU</language>' .
-     '<webMaster>' . Vars::$SYSTEM_SET['email'] . '</webMaster> ';
+    '<rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/"><channel>' .
+    '<title>' . htmlspecialchars(Vars::$SYSTEM_SET['copyright']) . ' | News</title>' .
+    '<link>' . Vars::$HOME_URL . '</link>' .
+    '<description>News</description>' .
+    '<language>ru-RU</language>' .
+    '<webMaster>' . Vars::$SYSTEM_SET['email'] . '</webMaster> ';
 
 // Новости
-$req = mysql_query('SELECT * FROM `news` ORDER BY `time` DESC LIMIT 15;');
-if (mysql_num_rows($req) > 0) {
-    while ($res = mysql_fetch_assoc($req)) {
+$req = DB::PDO()->query('SELECT * FROM `news` ORDER BY `id` DESC LIMIT 15');
+if ($req->rowCount()) {
+    while ($res = $req->fetch()) {
         echo '<item>' .
-             '<title>News: ' . $res['name'] . '</title>' .
-             '<link>' . Vars::$HOME_URL . 'news/</link>' .
-             '<author>' . $res['avt'] . '</author>' .
-             '<description>' . $res['text'] . '</description>' .
-             '<pubDate>' . date('r', $res['time']) .
-             '</pubDate>' .
-             '</item>';
+            '<title>News: ' . $res['name'] . '</title>' .
+            '<link>' . Vars::$HOME_URL . 'news/</link>' .
+            '<author>' . $res['avt'] . '</author>' .
+            '<description>' . $res['text'] . '</description>' .
+            '<pubDate>' . date('r', $res['time']) .
+            '</pubDate>' .
+            '</item>';
     }
 }
 
 // Библиотека
-$req = mysql_query("select * from `lib` where `type`='bk' and `moder`='1' order by `time` desc LIMIT 15;");
-if (mysql_num_rows($req) > 0) {
-    while ($res = mysql_fetch_array($req)) {
+$req = DB::PDO()->query("SELECT * FROM `lib` WHERE `type` = 'bk' AND `moder` = '1' ORDER BY `time` DESC LIMIT 15");
+if ($req->rowCount()) {
+    while ($res = $req->fetch()) {
         echo '<item>' .
-             '<title>Library: ' . $res['name'] . '</title>' .
-             '<link>' . Vars::$HOME_URL . 'library/?id=' . $res['id'] . '</link>' .
-             '<author>' . $res['avtor'] . '</author>' .
-             '<description>' . $res['announce'] .
-             '</description>' .
-             '<pubDate>' . date('r', $res['time']) . '</pubDate>' .
-             '</item>';
+            '<title>Library: ' . $res['name'] . '</title>' .
+            '<link>' . Vars::$HOME_URL . 'library/?id=' . $res['id'] . '</link>' .
+            '<author>' . $res['avtor'] . '</author>' .
+            '<description>' . $res['announce'] .
+            '</description>' .
+            '<pubDate>' . date('r', $res['time']) . '</pubDate>' .
+            '</item>';
     }
 }
 echo '</channel></rss>';
