808593d573b023f53018e1187c535e076ff0344f
AlkatraZ <AlkatraZ@d83bc508-5b26-e644-98a0-25f497cf4ffc>
2013-02-17 09:48:22 +0000

    [dev]   В форму Login добавлена CAPTCHA

--- modules/_users/_inc/login.php
+++ modules/_users/_inc/login.php
@@ -76,8 +76,34 @@ if (Vars::$USER_ID) {
 
     $tpl->form = $form->build();
 
+    // Обрабатываем CAPTCHA
+    if ($form->isSubmitted && ($user = Validate::getUserData()) !== FALSE) {
+        if ($user['login_try'] > 2) {
+            $captcha = TRUE;
+            $tpl->data = $form->output;
+            if (isset($_POST['captcha'])
+                && isset($_POST['form_token'])
+                && isset($_SESSION['form_token'])
+                && $_POST['form_token'] == $_SESSION['form_token']
+            ) {
+                if (Captcha::check() === TRUE) {
+                    $captcha = FALSE;
+                } else {
+                    $tpl->error = __('error_wrong_captcha');
+                }
+            }
+
+            if ($captcha) {
+                // Показываем форму CAPTCHA
+                $tpl->form_token = $_SESSION['form_token'];
+                $tpl->contents = $tpl->includeTpl('login_captcha');
+                exit;
+            }
+        }
+    }
+
+    // Авторизуем пользователя
     if ($form->isValid && ($user = Validate::getUserData()) !== FALSE) {
-        // Авторизуем пользователя
         //TODO: Добавить Капчу
 
         if (empty($user['token'])) {
@@ -103,111 +129,6 @@ if (Vars::$USER_ID) {
         header('Location: ' . Vars::$HOME_URL);
         exit;
     }
-
-//    $error = array();
-//    $autologin = FALSE;
-//    $value = FALSE;
-//    $sql = FALSE;
-
-//    $login_data['id'] = isset($_REQUEST['id']) ? trim($_REQUEST['id']) : FALSE;
-//    $login_data['token'] = isset($_REQUEST['token']) ? trim($_REQUEST['token']) : FALSE;
-//    $login_data['login'] = isset($_POST['login']) ? mb_substr(trim($_POST['login']), 0, 50) : FALSE;
-//    $login_data['password'] = isset($_POST['password']) ? mb_substr(trim($_POST['password']), 0, 50) : FALSE;
-
-//    if (Vars::$USER_SYS['autologin'] && $login_data['id'] > 0 && strlen($login_data['token']) == 32) {
-//        // Авторизация через ссылку AutoLogin
-//        $autologin = TRUE;
-//        $value = intval($_GET['id']);
-//        $sql = '`id` = ?';
-//    } elseif ($login_data['login'] !== FALSE && $login_data['password'] !== FALSE) {
-//        // Авторизация через форму
-//        $value = $login_data['login'];
-//        if (Validate::email($login_data['login']) === TRUE) {
-//            // Авторизация по E-mail адресу
-//            $sql = '`email` = ?';
-//        } elseif (Validate::nickname($login_data['login'], TRUE) === TRUE) {
-//            // Авторизация по Нику
-//            $sql = '`nickname` = ?';
-//        }
-//        Validate::password($login_data['password'], TRUE);
-//    }
-//
-//    $tpl->data = $login_data;
-//
-//    if ($sql && empty(Validate::$error)) {
-//        $STH = DB::PDO()->prepare('SELECT * FROM `users` WHERE ' . $sql . ' LIMIT 1');
-//        $STH->execute(array($value));
-//
-//        if ($STH->rowCount()) {
-//            $result = $STH->fetch();
-//            $token = empty($result['token']) ? Functions::generateToken() : $result['token'];
-//
-//            // Обрабатываем CAPTCHA
-//            if ($result['login_try'] > 2) {
-//                $captcha = TRUE;
-//                if (isset($_POST['captcha'])
-//                    && isset($_POST['form_token'])
-//                    && isset($_SESSION['form_token'])
-//                    && $_POST['form_token'] == $_SESSION['form_token']
-//                ) {
-//                    if (Captcha::check() === TRUE) {
-//                        $captcha = FALSE;
-//                    } else {
-//                        $error['captcha'] = __('error_wrong_captcha');
-//                    }
-//                }
-//
-//                if ($captcha) {
-//                    // Показываем форму CAPTCHA
-//                    $tpl->form_token = md5(mt_rand(1000, 100000) . microtime(TRUE));
-//                    $_SESSION['form_token'] = $tpl->form_token;
-//                    $tpl->error = $error;
-//                    $tpl->contents = $tpl->includeTpl('login_captcha');
-//                    exit;
-//                }
-//            }
-//
-//            if (($autologin && $login_data['token'] === $token)
-//                || (!$autologin && crypt($login_data['password'], $result['password']) === $result['password'])
-//            ) {
-//                // Если пароль, или токен совпадают, авторизуем пользователя
-//                $STHU = DB::PDO()->prepare('
-//                    UPDATE `users` SET
-//                    `login_try` = 0,
-//                    `token` = :token
-//                    WHERE `id` = :id
-//                ');
-//
-//                $STHU->bindValue(':token', $token, PDO::PARAM_STR);
-//                $STHU->bindValue(':id', $result['id'], PDO::PARAM_INT);
-//                $STHU->execute();
-//
-//                // Записываем сессию и COOKIE
-//                if (isset($_POST['remember'])) {
-//                    setcookie('uid', $result['id'], time() + 3600 * 24 * 31, '/');
-//                    setcookie('token', $token, time() + 3600 * 24 * 31, '/');
-//                }
-//                $_SESSION['uid'] = $result['id'];
-//                $_SESSION['token'] = $token;
-//
-//                // Пересылка на Главную страницу
-//                header('Location: ' . Vars::$HOME_URL);
-//                exit;
-//            } else {
-//                // Если пароль, или токен не совпадает
-//                $error['password'] = __('error_wrong_password');
-//                if ($result['login_try'] < 3) {
-//                    // Накручиваем счетчик неудачных Логинов
-//                    DB::PDO()->exec("UPDATE `users` SET `login_try` = " . ++$result['login_try'] . " WHERE `id` = " . $result['id']);
-//                }
-//            }
-//        } else {
-//            // Если пользователь не найден
-//            $error['login'] = __('error_user_not_exist');
-//        }
-//    }
-//
-//    $tpl->error = $error;
 }
 
 $tpl->contents = $tpl->includeTpl('login');
